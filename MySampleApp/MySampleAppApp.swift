44207bdce08671c254ad1d9fe060840393b5f270
unwithdrawal <mia000000chh@gmail.com>
2022-01-16 16:20:51 +0900

    Initial Commit

---
+++ MySampleApp/MySampleAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MySampleAppApp.swift
+//  MySampleApp
+//
+//  Created by 齋藤颯太 on 2022/01/16.
+//
+
+import SwiftUI
+
+@main
+struct MySampleAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MySampleAppTests/MySampleAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  MySampleAppTests.swift
+//  MySampleAppTests
+//
+//  Created by 齋藤颯太 on 2022/01/16.
+//
+
+import XCTest
+@testable import MySampleApp
+
+class MySampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
